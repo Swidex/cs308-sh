717b0e3c4ea7eeb5e784acc26dba6b9eb22b2845
Benjamin Hall <benhall@iastate.edu>
2022-10-05 12:59:25 -0500

    remove null space, more commands.

--- 308sh.c
+++ 308sh.c
@@ -1,42 +1,108 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <wordexp.h>
+
+#define MAX_ARGS 10
 
 int main (int argc, char* argv[])
 {
 	/*
 	Accept prompt option or
-	set to default "308sh>"
+	set to default "308sh> "
 	*/
 
 	char *prompt;
 	if (argc > 1 && strcmp(argv[1], "-p") == 0) {
 		prompt = argv[2];
 	} else {
-		prompt = "308sh>";
+		prompt = "308sh> ";
 	}
 
 	/*
-	Shell loop
+	Input loop
 	*/
+	char *cmd;
 
 	while (1)
 	{
-		//Accept user input
+		cmd = NULL;
+		// Accept user input
 		char *uinput;
 		printf("%s",prompt); // print prompt
 		fgets(uinput, 64, stdin);
 
-		//Process user input
-		if (strcmp(uinput, "exit\n") == 0) {
-			printf("Exiting 308sh.\n");
-			return 1;
-		} else if (strcmp(uinput, "pid\n") == 0) {
+		// Parse command and arguments
+		if (uinput[0] != '\n') // don't cut if empty line
+			uinput[strcspn(uinput, "\n")] = 0; // remove trailing newline for ease of parsing.
+		
+		cmd = strtok(uinput, " ");
+
+		// Process user input
+		if (strcmp(cmd, "exit") == 0) {
+			// terminate process
+			return 0;
+
+		} else if (strcmp(cmd, "pid") == 0) {
+			// print process id
 			printf("%d\n", getpid());
+
+		} else if (strcmp(cmd, "ppid") == 0) {
+			// print parent process id
+			printf("%d\n", getppid());
+
+		} else if (strcmp(cmd, "pwd") == 0) {
+			// print current working directory
+			char* buf;
+			if ( ( buf = getcwd(NULL, 0)) == NULL )
+			{
+				perror("Failed to get current directory.\n");
+			} else {
+				printf("%s\n", buf);
+				free(buf);
+			}
+
+		} else if (strcmp(cmd, "cd") == 0) {
+			// change directory (defaults to $HOME if no arguments specified)
+			cmd = strtok(NULL, " "); // grab next argument
+			if (cmd == NULL) {
+				if (chdir(getenv("HOME")) != 0) perror("Failed to change to home path.\n");
+			} else {
+				if (chdir(cmd) != 0) perror("Failed to change path.\n");
+			}
+
+		} else if (strcmp(cmd, "\n") == 0 ) {
+			// do nothing
+
 		} else {
-			printf("Unknown command.\n");
+			// execute file
+			if (cmd == NULL) {
+				perror("No path specified.\n");
+				continue;
+			}
+
+			// seperate arguments
+			int i = 0;
+			int stat;
+			char *arg_list[MAX_ARGS];
+
+			while (uinput != NULL) {
+				arg_list[i++] = uinput;
+				uinput = strtok(NULL," ");
+			}
+			arg_list[i++] = NULL;
+
+			int pid = fork();
+			if (pid == 0) {
+				execvp(arg_list[0], arg_list);
+				err(1, "execvp");
+			} else {
+				waitpid(pid, &stat, 0);
+				fprintf(stderr, "child(%d) exited with %d\n",pid, WEXITSTATUS(stat));
+			}
 		}
 	}
 
-	return 1;
+	return 0;
 }
 
