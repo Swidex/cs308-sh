5af8be8c9889f51d78129d5a8f95fc84fb69d4c7
Benjamin Hall <benhall@iastate.edu>
2022-10-05 20:48:06 -0500

    seperated functions, added watcher processes for bg tasks.

--- 308sh.c
+++ 308sh.c
@@ -1,103 +1,169 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <wordexp.h>
+#include <wait.h>
+#include <assert.h>
 
 #define MAX_ARGS 10
+#define MAX_JOBS 2
+
+
+char *prompt;
+
+
+char** tokenize(char* a_str, const char a_delim)
+{
+	char** res			= 0;
+	size_t cnt			= 0;
+	char* tmp			= a_str;
+	char* last_delim	= 0;
+	char delim[2];
+	delim[0] = a_delim;
+	delim[1] = 0;
+	
+	// count number of arguments
+	while (*tmp){
+		if (a_delim == *tmp) {
+			cnt++;
+			last_delim = tmp;
+		}
+		tmp++;
+	}
+
+	cnt += last_delim < (a_str + strlen(a_str) - 1);
+	cnt++;
+	res = malloc(sizeof(char*) * cnt);
+
+	if (res) {
+		size_t idx	= 0;
+		char* token = strtok(a_str, delim);
+
+		while (token) {
+			assert(idx < cnt);
+			*(res + idx++) = strdup(token);
+			token = strtok(0, delim);
+		}
+		assert(idx == cnt - 1);
+		*(res + idx) = 0;
+	}
+	return res;
+}
+
+int processTokens(char** tokens)
+{
+	if (strcmp(*(tokens), "exit") == 0) {
+		// terminate process
+		return 1;
+
+	} else if (strcmp(*(tokens), "pid") == 0) {
+		// print process id
+		printf("%d\n", getpid());
+
+	} else if (strcmp(*(tokens), "ppid") == 0) {
+		// print parent process id
+		printf("%d\n", getppid());
+
+	} else if (strcmp(*(tokens), "pwd") == 0) {
+		// print current working directory
+		char* buf;
+		if ( ( buf = getcwd(NULL, 0)) == NULL )
+		{
+			perror("Failed to get current directory.\n");
+		} else {
+			printf("%s\n", buf);
+			free(buf);
+		}
+
+	} else if (strcmp(*(tokens), "cd") == 0) {
+		// change directory (defaults to $HOME if no arguments specified)
+		if (*(tokens + 1) == NULL) {
+			if (chdir(getenv("HOME")) != 0) perror("Failed to change to home path.\n");
+		} else {
+			if (chdir(*(tokens + 1)) != 0) perror("Failed to change path.\n");
+		}
+
+	} else if (strcmp(*(tokens), "") == 0 ) {
+		// do nothing
+
+	} else {
+		/* Execute file */
+		// set to background if flagged
+		int stat;
+		int i, vargc, bg = 0;
+		for (vargc = 0; *(tokens+vargc); vargc++);
+		if (strcmp(*(tokens+vargc-1),"&") == 0) bg = 1;
+
+		int wid = fork();
+		if (wid == 0) {
+			int pid = fork();
+			if (pid == 0) {
+				/* Child Process */
+				pid = getpid();
+				
+				/* Announce execution */
+				printf("child(%d) executing (", pid);
+				for (i = 0; *(tokens+i); i++) printf(" %s",*(tokens+i));
+				if (bg) {
+					printf(" ) in background.\n");
+				} else { printf(" )\n"); }
+
+				char *vargs[vargc+1];
+				for (i = 0; i <= vargc; i++) {
+					vargs[i] = *(tokens+i);
+				}
+				vargs[vargc] = NULL;
+				if (vargs[vargc - 1] == "&") vargs[vargc - 1] = NULL;
+
+				execvp(vargs[0], vargs);
+				err(1, "execvp");
+			} else {
+				// watcher process
+				waitpid(pid, &stat, 0);
+				fprintf(stderr, "\nchild(%d) exited with status %d\n",pid, WEXITSTATUS(stat));
+			}
+		} else {
+			if (bg) {
+				sleep(1);
+				return 0;
+			} else {
+				wait(NULL);
+			}
+		}
+	}
+	return 0;
+}
 
 int main (int argc, char* argv[])
 {
-	/*
-	Accept prompt option or
-	set to default "308sh> "
-	*/
-
-	char *prompt;
+	/* Set prompt flag */
 	if (argc > 1 && strcmp(argv[1], "-p") == 0) {
 		prompt = argv[2];
 	} else {
 		prompt = "308sh> ";
 	}
 
-	/*
-	Input loop
-	*/
-	
+	/* Main Loop */
 	while (1)
 	{
-		fflush(stdin);
 		printf("%s",prompt); // print prompt
 
-		// Accept user input
-		char *cmd;
+		/* Receive user input from stdin */
+		char **tokens;
 		char *uinput;
+		int bg = 0;
 		fgets(uinput, 64, stdin);
-
-		// Parse command and arguments
-		if (uinput[0] != '\n') // don't cut if empty line
-			uinput[strcspn(uinput, "\n")] = 0; // remove trailing newline for ease of parsing.
+		uinput[strcspn(uinput, "\n")] = 0; // clear newline
 		
-		cmd = strtok(uinput, " ");
+		tokens = tokenize(uinput, ' ');
+		if (tokens) {
+			processTokens(tokens);
 
-		// Process user input
-		if (strcmp(cmd, "exit") == 0) {
-			// terminate process
-			return 0;
-
-		} else if (strcmp(cmd, "pid") == 0) {
-			// print process id
-			printf("%d\n", getpid());
-
-		} else if (strcmp(cmd, "ppid") == 0) {
-			// print parent process id
-			printf("%d\n", getppid());
-
-		} else if (strcmp(cmd, "pwd") == 0) {
-			// print current working directory
-			char* buf;
-			if ( ( buf = getcwd(NULL, 0)) == NULL )
-			{
-				perror("Failed to get current directory.\n");
-			} else {
-				printf("%s\n", buf);
-				free(buf);
+			/* Free tokens from memory */
+			int i;
+			for (i = 0; *(tokens + i); i++) {
+				free(*(tokens + i));
 			}
-
-		} else if (strcmp(cmd, "cd") == 0) {
-			// change directory (defaults to $HOME if no arguments specified)
-			cmd = strtok(NULL, " "); // grab next argument
-			if (cmd == NULL) {
-				if (chdir(getenv("HOME")) != 0) perror("Failed to change to home path.\n");
-			} else {
-				if (chdir(cmd) != 0) perror("Failed to change path.\n");
-			}
-
-		} else if (strcmp(cmd, "\n") == 0 ) {
-			// do nothing
-
-		} else {
-			// execute file
-			int stat;
-			int pid = fork();
-			if (pid == 0) {
-				pid = getpid();
-				printf("[PID:%d] running [%s]\n", pid, cmd);
-				
-				// seperate arguments
-				int i = 0;
-				char *arg_list[MAX_ARGS];
-				while (uinput != NULL) {
-					arg_list[i++] = uinput;
-					uinput = strtok(NULL," ");
-				}
-				arg_list[i++] = NULL;
-
-				execvp(arg_list[0], arg_list);
-				err(1, "execvp");
-			} else {
-				waitpid(pid, &stat, 0);
-				fprintf(stderr, "child(%d) exited with %d\n",pid, WEXITSTATUS(stat));
-			}
+			free(tokens);
 		}
 	}
 
